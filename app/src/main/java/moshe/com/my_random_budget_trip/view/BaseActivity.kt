5aa697e3ee1701b4d30404f9a8784d3f8b062a95
Moshe Shimon <moshe31sh@gmail.com>
2018-01-09 16:58:06 +0200

    ADD ANIMATION

--- app/src/main/java/moshe/com/my_random_budget_trip/view/BaseActivity.kt
+++ app/src/main/java/moshe/com/my_random_budget_trip/view/BaseActivity.kt
@@ -2,6 +2,7 @@ package moshe.com.my_random_budget_trip.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import moshe.com.my_random_budget_trip.R
 
 /**
  * Created by moshe on 09/01/2018.
@@ -16,4 +17,22 @@ open class BaseActivity : AppCompatActivity() {
 
     open fun getContentView() = 0
 
+
+    protected fun showLoadingDialog(){
+
+        TODO("build loading dialog")
+    }
+
+
+    protected fun dismissLoadingDialog(){
+        TODO("build loading dialog")
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if(!isTaskRoot) {
+            overridePendingTransition(R.anim.anim_slide_out_right, R.anim.anim_slide_in_right)
+        }
+    }
 }
--- app/src/main/java/moshe/com/my_random_budget_trip/view/LoginActivity.kt
+++ app/src/main/java/moshe/com/my_random_budget_trip/view/LoginActivity.kt
@@ -2,14 +2,40 @@ package moshe.com.my_random_budget_trip.view
 
 import android.os.Bundle
 import moshe.com.my_random_budget_trip.R
+import moshe.com.my_random_budget_trip.contract_impl.LoginPresenterImpl
+import moshe.com.my_random_budget_trip.contracts.ILoginPresenter
+import moshe.com.my_random_budget_trip.contracts.ILoginView
 
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(), ILoginView {
+
+    private lateinit var mLoginPresenterImpl: ILoginPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mLoginPresenterImpl = LoginPresenterImpl(this)
     }
 
     override fun getContentView(): Int {
         return R.layout.activity_login
+    }
+
+    override fun showLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showUserNameError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideLoading() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showPasswordError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSuccess() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
