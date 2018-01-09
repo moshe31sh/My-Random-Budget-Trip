7b9613eb5d3a8dc3973dcc3d00d024e7aafe4353
Moshe Shimon <moshe31sh@gmail.com>
2018-01-09 16:03:26 +0200

    first commit

---
+++ app/src/androidTest/java/moshe/com/my_random_budget_trip/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package moshe.com.my_random_budget_trip
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("moshe.com.my_random_budget_trip", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/moshe/com/my_random_budget_trip/LoginActivity.kt
@@ -0,0 +1,15 @@
+package moshe.com.my_random_budget_trip
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getContentView(): Int {
+        return R.layout.activity_login
+    }
+}
