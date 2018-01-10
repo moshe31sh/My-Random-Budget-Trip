ed07db27e900d2153a3dfd3fb4ed83d5a657ed04
Moshe Shimon <moshe31sh@gmail.com>
2018-01-10 08:52:33 +0200

    create register activity

---
+++ app/src/main/java/moshe/com/my_random_budget_trip/view/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package moshe.com.my_random_budget_trip.view
+
+import android.os.Bundle
+import moshe.com.my_random_budget_trip.R
+
+class RegisterActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getContentView(): Int = R.layout.activity_register
+}
