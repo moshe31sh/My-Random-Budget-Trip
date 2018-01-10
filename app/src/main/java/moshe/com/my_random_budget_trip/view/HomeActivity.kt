4588c4def6f5bcf35d248eec0b571b9feff19ae8
Moshe Shimon <moshe31sh@gmail.com>
2018-01-10 11:08:18 +0200

    create home activity

---
+++ app/src/main/java/moshe/com/my_random_budget_trip/view/HomeActivity.kt
@@ -0,0 +1,15 @@
+package moshe.com.my_random_budget_trip.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import moshe.com.my_random_budget_trip.R
+
+class HomeActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getContentView(): Int = R.layout.activity_home
+}
+
